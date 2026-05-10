54003e37c299e935459af5c93ac77ce5d7a827e5
Ghaleb Hamadi <ghaleb@gtlogic.com>
2026-05-10 18:19:12 +0300

    add button-group component

---
+++ web/src/components/ui/separator.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import * as React from 'react';
+import { Separator as SeparatorPrimitive } from 'radix-ui';
+
+import { cn } from '@web/lib/utils/components';
+
+export type SeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root>;
+
+export function Separator(props: SeparatorProps) {
+  const { className, orientation = 'horizontal', decorative = true, ...rest } = props;
+  return (
+    <SeparatorPrimitive.Root
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        'bg-border shrink-0 data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch',
+        className,
+      )}
+      {...rest}
+    />
+  );
+}
